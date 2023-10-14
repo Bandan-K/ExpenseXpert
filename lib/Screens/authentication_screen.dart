91130ebe4875e459baf16c3bd8f35e980516b9b7
Bandan-K <bandan.kashish@gmail.com>
2023-10-14 09:57:52 +0530

    Google SignIn Added and Modified

--- lib/Screens/authentication_screen.dart
+++ lib/Screens/authentication_screen.dart
@@ -1,6 +1,5 @@
 import 'package:expensexpert/homepage.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
